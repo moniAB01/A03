6b33ba1a3401ff7d5093244186ac517c9b454d1b
Glitch (glitch-hello-website) <none>
2021-10-25 10:11:35 +0000

    🎉🌘 Checkpoint ./script.js:36096504/108 ./style.css:36096504/41 ./index.html:36096504/1

--- script.js
+++ script.js
@@ -47,22 +47,24 @@ const strText = text.textContent;
 const splitText = strText.split("");
 text.textContent ="";
 for(let i=0; i < splitText.length; i++){
-  text.innerHTML += "<span>" + splitT[i] + "</span>";
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 let char = 0;
 let timer = setInterval(onTick, 50);
 
 function onTick(){
-  const span = text.querySelectoryAll('span')[char];
+  const span = text.querySelectorAll('span')[char];
   span.classList.add('fade');
   char++
-  if(char === spliting.length){
+  if(char === splitText.length){
     complete();
     return;
   }
+  console.log("runthrough")
 }
 
 function complete(){
   clearInterval(timer);
-  timer = null;
+  timer = 50;
+  console.log("secondtime")
 }
